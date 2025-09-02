1396d8b1a5fb3dbd47a989b5f0c76fc64639148f
Sergey Slotin <me@sereja.me>
2025-09-02 05:59:27 +0300

    add more test examples

--- scanner/test.js
+++ scanner/test.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import { parse } from './scanner.js';
 
-const imagePath = '../test-screenshots/bunker.png';
+//const imagePath = '../test-screenshots/seaport.png';
+const imagePath = '../test-screenshots/short.png';
 const imageData = fs.readFileSync(imagePath);
 const result = await parse(imageData);
 //console.log(result);
